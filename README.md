a25119bf6597cb310e9c37ea2186a3d9949edf6f
Brandon Cowley <brandon.cowley@gmail.com>
2021-05-02 21:51:12 -0700

    Project completed, HTML final udates, JS being pushed to repo

--- README.md
+++ README.md
@@ -1,90 +1,17 @@
 # 06 Server-Side APIs: Weather Dashboard
 
-## Your Task
+For this project, I was given the task of making a a weather app that can display the current weather, using a variety of metrics including wind, precipitation, and UV Index. The app also had to show a 5 day projection of weather for the dselected city. 
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+In completing this project, I not only hav ea functioning site, but something that can have an everyday use, and can also serve as a good reference in the future when working with APIs.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that provides basic setup and usage instructions. You will use `localStorage` to store any persistent data.
+In building this page and designing it, I gained a huge appreciation for the complexities of what I have always assumed were very basic widgets or apps tht I use every day! I did not have enought time to style the page fully in the way that I would have liked, but I spent a lot of time researching how to pull data from an API.
 
-## User Story
+One key takeaway that I learned this week was that reading the documentation that goes along with an API is essential to it being successful. Without knowing the syntax of how to order your requests, it is very easy to be lost so close to the finish line. I am also getting better about taking/eaving notes in my code. Prior to this I had been able to visualize the "complexities" of the site as they were mostly visual. Now that we are getting more into back-end knowledge, it is essential to write out your actions before jumping into the code sheets.
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+If I had to do this project again, I would do two things. One would be to improve the UI of the page. I spent too much time learning how to complete the JS sheet that I did not get time to work on the looks. As a result, it feels more basic than I would like. The second thing I would do is take more time up front and write out my workflow in the HTML/JS sheets. Preparation there would stop be from slipping into analysis paralysis, and focus on small progressions. 
 
-## Acceptance Criteria
+LINK TO LIVE PROJECT:
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+https://brandoncowley.github.io/WeatherDashboard/
 
-## Mock-Up
-
-The following image shows the web application's appearance and functionality:
-
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+![Screenshot of the functioning app](./WeatherAppScreenshot.JPG)
--- script.js
+++ script.js
@@ -1,4 +1,138 @@
-//Use OpenWeather API to get weather info for cities
-var requestUrl = "https://openweathermap.org/api"
+//Variables needed = city, search, clear, 
+//temp, humidity, wind, UV, future forecast
+const cityEl = document.getElementById("enter-city");
+const searchEl = document.getElementById("search-button");
+const clearEl = document.getElementById("clear-history");
+const nameEl = document.getElementById("city-name");
+const currentPicEl = document.getElementById("current-pic");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+const historyEl = document.getElementById("history");
+var fivedayEl = document.getElementById("fiveday-header");
+var todayweatherEl = document.getElementById("today-weather");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-APIKey = "4522a7b988b7fe26751a3d7a52f304ab"
+// Use the API key here
+const APIKey = "4522a7b988b7fe26751a3d7a52f304ab";
+
+function getWeather(cityName) {
+    // Pull the current weather from OpenWeather API
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + APIKey;
+    axios.get(queryURL)
+        .then(function (response) {
+
+            todayweatherEl.classList.remove("d-none");
+
+            // Parse response to display current weather
+            const currentDate = new Date(response.data.dt * 1000);
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth() + 1;
+            const year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + (response.data.main.temp) + " &#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+            
+            // Get UV Index
+            let lat = response.data.coord.lat;
+            let lon = response.data.coord.lon;
+            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+            axios.get(UVQueryURL)
+                .then(function (response) {
+                    let UVIndex = document.createElement("span");
+                    
+                    // When UV Index is good, shows green, when ok shows yellow, when bad shows red
+                    if (response.data[0].value < 4 ) {
+                        UVIndex.setAttribute("class", "badge badge-success");
+                    }
+                    else if (response.data[0].value < 8) {
+                        UVIndex.setAttribute("class", "badge badge-warning");
+                    }
+                    else {
+                        UVIndex.setAttribute("class", "badge badge-danger");
+                    }
+                    
+                    UVIndex.innerHTML = response.data[0].value;
+                    currentUVEl.innerHTML = "UV Index: ";
+                    currentUVEl.append(UVIndex);
+                });
+            
+            // Get 5 day forecast for the chosen city
+            let cityID = response.data.id;
+            let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=imperial" + "&appid=" + APIKey;
+            axios.get(forecastQueryURL)
+                .then(function (response) {
+                    fivedayEl.classList.remove("d-none");
+                    
+                    //  Make it possible to display forecast for next 5 days
+                    const forecastEls = document.querySelectorAll(".forecast");
+                    for (i = 0; i < forecastEls.length; i++) {
+                        forecastEls[i].innerHTML = "";
+                        const forecastIndex = i * 8 + 4;
+                        const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                        const forecastDay = forecastDate.getDate();
+                        const forecastMonth = forecastDate.getMonth() + 1;
+                        const forecastYear = forecastDate.getFullYear();
+                        const forecastDateEl = document.createElement("p");
+                        forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
+                        forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
+                        forecastEls[i].append(forecastDateEl);
+
+                        // Find and DISPLAY icon for current weather
+                        const forecastWeatherEl = document.createElement("img");
+                        forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                        forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                        forecastEls[i].append(forecastWeatherEl);
+                        const forecastTempEl = document.createElement("p");
+                        forecastTempEl.innerHTML = "Temp: " + (response.data.list[forecastIndex].main.temp) + " &#176F";
+                        forecastEls[i].append(forecastTempEl);
+                        const forecastHumidityEl = document.createElement("p");
+                        forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                        forecastEls[i].append(forecastHumidityEl);
+                    }
+                })
+        });
+}
+
+// When you click "Search", push text to local storage
+searchEl.addEventListener("click", function () {
+    const searchTerm = cityEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    renderSearchHistory();
+})
+
+// Clear History button
+clearEl.addEventListener("click", function () {
+    localStorage.clear();
+    searchHistory = [];
+    renderSearchHistory();
+})
+
+//items from search history need to be displayed in a list
+function renderSearchHistory() {
+    historyEl.innerHTML = "";
+    for (let i = 0; i < searchHistory.length; i++) {
+        const historyItem = document.createElement("input");
+        historyItem.setAttribute("type", "text");
+        historyItem.setAttribute("readonly", true);
+        historyItem.setAttribute("class", "form-control d-block bg-white");
+        historyItem.setAttribute("value", searchHistory[i]);
+        historyItem.addEventListener("click", function () {
+            getWeather(historyItem.value);
+        })
+        historyEl.append(historyItem);
+    }
+}
+
+renderSearchHistory();
+if (searchHistory.length > 0) {
+    getWeather(searchHistory[searchHistory.length - 1]);
+}
+ 
